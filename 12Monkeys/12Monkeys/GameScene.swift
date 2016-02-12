d84b4dcdefbf8c234575a346fd82ec86c1e9c490
Kevia <cloud@kevia.me>
2016-02-12 15:09:56 -0800

    last game with Monkeys

---
+++ 12Monkeys/12Monkeys/GameScene.swift
@@ -0,0 +1,27 @@
+//
+//  GameScene.swift
+//  12Monkeys
+//
+//  Created by jhampac on 2/12/16.
+//  Copyright (c) 2016 jhampac. All rights reserved.
+//
+
+import SpriteKit
+
+class GameScene: SKScene
+{
+    override func didMoveToView(view: SKView)
+    {
+        
+    }
+    
+    override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?)
+    {
+      
+    }
+   
+    override func update(currentTime: CFTimeInterval)
+    {
+        
+    }
+}
