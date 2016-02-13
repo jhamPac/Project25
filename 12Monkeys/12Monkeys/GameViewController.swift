acc6a33266fd967157b110a2d57a698287e2165e
Kevia <cloud@kevia.me>
2016-02-12 17:58:25 -0800

    one profound thing adding a reference between VC and GameScene

--- 12Monkeys/12Monkeys/GameViewController.swift
+++ 12Monkeys/12Monkeys/GameViewController.swift
@@ -9,8 +9,38 @@
 import UIKit
 import SpriteKit
 
-class GameViewController: UIViewController {
-
+class GameViewController: UIViewController
+{
+    var currentGame: GameScene!
+    
+    @IBOutlet weak var angleSlider: UISlider!
+    @IBOutlet weak var angleLabel: UILabel!
+    @IBOutlet weak var velocitySlider: UISlider!
+    @IBOutlet weak var velocityLabel: UILabel!
+    @IBOutlet weak var launchButton: UIButton!
+    @IBOutlet weak var playerNumber: UILabel!
+    
+    @IBAction func angleChanged(sender: UISlider)
+    {
+        angleLabel.text = "Angle: \(Int(angleSlider.value))"
+    }
+    
+    @IBAction func velocityChanged(sender: UISlider)
+    {
+        velocityLabel.text = "Velocity: \(Int(velocitySlider.value))"
+    }
+    
+    @IBAction func launch(sender: UIButton)
+    {
+        angleSlider.hidden = true
+        angleLabel.hidden = true
+        velocitySlider.hidden = true
+        velocityLabel.hidden = true
+        
+        launchButton.hidden = true
+        currentGame.launch(angle: Int(angleSlider.value), velocity: Int(velocitySlider.value))
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -27,6 +57,12 @@ class GameViewController: UIViewController {
             scene.scaleMode = .AspectFill
             
             skView.presentScene(scene)
+            
+            currentGame = scene
+            scene.viewController = self
+            
+            angleChanged(angleSlider)
+            velocityChanged(velocitySlider)
         }
     }
 
@@ -42,12 +78,23 @@ class GameViewController: UIViewController {
         }
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Release any cached data, images, etc that aren't in use.
-    }
-
     override func prefersStatusBarHidden() -> Bool {
         return true
+    }
+    
+    func activatePlayerNumber(number: Int) {
+        if number == 1 {
+            playerNumber.text = "<<< PLAYER ONE"
+        } else {
+            playerNumber.text = "PLAYER TWO >>>"
+        }
+        
+        angleSlider.hidden = false
+        angleLabel.hidden = false
+        
+        velocitySlider.hidden = false
+        velocityLabel.hidden = false
+        
+        launchButton.hidden = false
     }
 }
